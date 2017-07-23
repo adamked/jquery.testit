bb0b36319f4384fdd29e32c4b1b53e169ad8adf3
adamked <quangthat97@gmail.com>
2017-07-24 03:59:37 +0700

    Add new file jquery.testit.js

---
+++ jquery.testit.js
@@ -0,0 +1,11 @@
+/*
+ * name : adamked
+ * author: Nguyen Dang Quang
+ * version: 0.2.0
+ * license: MIT
+ */
+($function($) {
+    $.fn.testitit = function() {
+        alert(1);
+    }
+}(jQuery));
